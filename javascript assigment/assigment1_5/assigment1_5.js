c09d78af5a92b001ff110b05ca1096e08fa5300a
Syed Zubair hashmi <156568348+SyedZubairHashmi@users.noreply.github.com>
2024-03-18 04:38:13 +0500

    Add files via upload

---
+++ javascript assigment/assigment1_5/assigment1_5.js
@@ -0,0 +1,238 @@
+                                                                   //    assiment 01
+// var messege = "Error! Please enter a valid password";
+// alert(messege);
+
+// var messege2 = "Welcome to JS Land\nHappy Coding! ";
+// alert(messege2)
+
+// var messegeA1 = "Welcome to JS Land";
+// var messegeA2 = "Happ Coding!\nPrevent the page from creating additional dialog";
+// alert(messegeA1);
+// alert(messegeA2);
+                                                                       //  assigment 02
+
+// var username ;
+// var myName = "zubair hashmi";
+// var messege = "Hello World";
+// alert(messege);
+//  var name = "Jhone Doe";
+//  var birth = "15 year old";
+//  var skill = "Certified Mobile pplication Development";
+//  alert(name);
+//  alert(birth);
+//  alert(skill);
+
+// var piza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(piza);
+
+
+// var email = "My email address is zubairhashmi423@gmail.com";
+// alert(email);
+
+// var book = "A amarter way to learn javascript";
+// alert(book);
+
+// document.write("Yeah ! I can write HTML content through Javascript");
+
+                                                                        //    assigment 03
+// var age= "I am 15 year old";
+// alert(age);
+
+                                                                        //    question 02
+// var visit = "you visit 14 time this page";
+// alert(visit);
+                                                                        //        quetion 03
+// var birthYear = 2004;
+// document.write("my birth year is ",birthYear);
+// document.write("Data tpe of my declared variable is ", typeof  birthYear);
+                                                                         //        quetion 04
+
+// var name = prompt("what is your name");
+// var title = prompt("what product you want");
+// var quantitiy = prompt("what is the quantity of your product you order")
+// document.writeln(name+ " orderd " + quantitiy + " " + title + " from milinium");
+            
+                                                                            //    assigment 04
+
+// declare 3 variable in one statement                                                                           
+// var name , dateOfBirth , address;
+// // declare 5 legalle variable
+// var  myName;
+// var nickName;
+// var _underscoreVar;
+// var camelCaseVar;
+// var $dollarVar;
+
+// declare illigale variable
+
+// var 123illegalVar;
+// var my-var;
+// var var with space;
+// var first-name;
+// var if; // reserved keyword
+
+
+                                                                   //    assigment 05
+
+
+                                                                //    write a progrma that take two number and add it in new variable
+    
+// var number1 = parseFloat(prompt("enter 1  number for addition"));
+// var number2 = parseFloat(prompt("enter 2 number for addition"));
+// var addition = number1 + number2;
+// document.write("addition of 1 and 2 number is = " + addition);
+
+// . Repeat task1 for subtraction, multiplication, division & modulus.
+
+// var subtraction = number1 - number2;
+// document.write("subtraction of 1 and 2 number is = " + subtraction);
+
+// var multiplication = number1 * number2;
+// document.write("Multiplication of 1 and 2 number is = " + multiplication);
+
+// var division = number2/number1;
+// document.write("division of 1 and 2 number is = " + division);
+
+
+// var modulus = number2%number1;
+// document.write("modulus of 1 and 2 number is = " + modulus);
+
+                                 // question 03
+// var statement = ("Value after variable declaration is : ??")
+// document.write(statement)      
+
+// var number0;
+// document.write("Value after variable declaration is ", number0, "<br>");
+// number0 = 14;
+// document.write("Initial value: ", number0,"<br>");
+
+// number0++;
+// document.write("Value after increment is: " ,number0, "<br>");
+// number0 += 7;
+// document.write("Value after addition 7 is: ", number0, "<br>");
+// number0--;
+// document.write("Value after decrement is: ", number0, "<br>");
+// document.write("The remainder is: ",number0 % 3 , "<br>");
+
+                                 //   Question 04
+// var ticket = 600;
+// var quantitiyTicket = prompt("Enter how many ticket you want");
+// document.write(`Total cost to buy ${quantitiyTicket} tickets to movie is ${ticket*quantitiyTicket}PKR`)                                
+
+                                  //   Question 04                                   
+                                       
+// var table = prompt("Enter table you want to print");
+// document.write("Table of ",table)
+// document.write(`${table}X1=${table*1} <br>`)
+// document.write(`${table}X2=${table*2} <br>`)
+// document.write(`${table}X3=${table*3}<br>`)
+// document.write(`${table}X4=${table*4}<br>`)
+// document.write(`${table}X5=${table*5}<br>`)
+// document.write(`${table}X6=${table*6}<br>`)
+// document.write(`${table}X7=${table*7}<br>`)
+// document.write(`${table}X8=${table*8}<br>`)
+// document.write(`${table}X9=${table*9}<br>`)
+// document.write(`${table}X10=${table*10}<br>`)
+
+
+                                 //  Question 06
+// var temperatureInCelsius = 35;
+// var temperatureInFahrenheit = (temperatureInCelsius * 9/5) + 32;
+// document.write(`${temperatureInCelsius}C is ${temperatureInFahrenheit}F <br> `)
+// var convertFinC = (temperatureInCelsius * 9/5) +32;
+// document.write(`${temperatureInFahrenheit}F is ${temperatureInCelsius}C <br>`)
+
+
+                                 // Question 07
+// Write a program to implement checkout process of a 
+// shopping cart system for an e-commerce website. Store 
+// the following in variables
+
+// var price01 = 30;
+// var price02 = 200;
+// var quantitiy01 = prompt("Enter quantity of item01 ")
+// var quantitiy02 = prompt("Enter quantity of item02 ")
+// document.write(`Price of item 1 is : ${price01}<br>`)
+// document.write(`Quantity of item 1 is : ${quantitiy01}<br>`)
+// document.write(`Price of item 2 is : ${price02}<br>`)
+// document.write(`Quantity of item 2 is : ${quantitiy02}<br>`)
+// document.write(`Total cost of your order is : ${(price01*quantitiy01)+(price02*quantitiy02)}<br>`)
+             
+                                     // Question 08
+// Store total marks & marks obtained by a student in 2 
+// variables. Compute the percentage & show the result in 
+// your browser
+
+// var totalMarks = 550;
+// var marksObtained = 432;
+// document.write(`Total marks: ${totalMarks} <br>`)
+// document.write(`Marks obtained: ${marksObtained} <br>`)
+// document.write(`Percentage: ${marksObtained/totalMarks*100}`)
+
+
+                                     // Question 09
+
+//  Assume we have 10 US dollars & 25 Saudi Riyals. Write a 
+// script to convert the total currency to Pakistani Rupees. 
+// Perform all calculations in a single expression.
+// (Exchange rates : 1 US Dollar = 104.80 Pakistani Rupee 
+// and 1 Saudi Riyal = 28 Pakistani Rupee)
+
+// var dollar = 104.80;
+// var SaudiRiyal = 28;
+// document.write(`Total Currency in PKR : ${10*dollar+25*SaudiRiyal}`)
+
+                                    // Question 10
+// 10. Write a program to initialize a variable with some 
+// number and do arithmetic in following sequence:
+// a. Add 5
+// b. Multiply by 10
+// c. Divide the result by 2
+// Perform all calculations in a single expression
+
+
+
+// var number4 = 15;
+// document.write(`${(number4+5)*10/2}`)
+                                    
+                                        // Question 11
+// var currentYear = 2024;
+// var birthYear = 2002;
+// document.write(`Current year: ${currentYear}<br>`)
+// document.write(`Birth year: ${birthYear}<br>`)
+// document.write(`Your age is : ${currentYear-birthYear}<br>`)
+
+
+
+                                       // Question 12
+// 12. The Geometrizer: Calculate properties of a circle.
+// a. Store a radius into a variable.
+// MATH EXPRESSIONS | JAVASCRIPT
+// Page 8 of 9
+// b. Calculate the circumference based on the radius, and
+// output “The circumference is NN”.
+// (Hint : Circumference of a circle = 2 π r , π = 3.142)
+// Calculate the area based on the radius, and output “The 
+// area is NN”. (Hint : Area of a circle = π r2, π = 3.142)
+        
+
+// var r = 3;
+// document.write(`Radius of Circle: ${r}<br>`)
+// document.write(`The circumference is ${2*3.142*r}<br>`)
+// document.write(`The Area is: ${3.142*(r*r)}<br>`)
+
+
+
+                                         // Question 13
+// document.write("The Lifetime Supply Calculator")                                        
+// var snack = "chocolate chip";
+// var currentAge = 22;
+// var maxAge = 70;
+// var perDay = 8;
+// document.write(`Favorite Snack : ${snack }<br>`)
+// document.write(`Current age :${currentAge }<br>`)
+// document.write(`Estimated Maximum Age :${maxAge }<br>`)
+// document.write(`Amount of snaks per day :${perDay }<br>`)
+
+// document.write(`You will need ${(currentAge-maxAge)*perDay} chocolate chip to last you untill the ripe old age of ${maxAge}`)                                         
+
