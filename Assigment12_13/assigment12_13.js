b33ef77be785abf46882c73c7b8c035c5cbc8f35
Syed Zubair hashmi <156568348+SyedZubairHashmi@users.noreply.github.com>
2024-03-20 10:05:42 +0500

    Add files via upload

---
+++ Assigment12_13/assigment12_13.js
@@ -0,0 +1,104 @@
+                //  question 01
+
+// var char = prompt("Enter a number 65 && 122")
+// if(char >= 65 && char <= 90 ){
+//     document.write("Charactor is upper case")
+// }else if(char >= 97 && char <= 122){
+//     document.write("Charactor is lower case")
+// }else{
+//     document.write("you enter out charector")
+// }
+
+                //   question 02
+//  Write a JavaScript program that accept two integers and
+// display the larger. Also show if the two integers are equal.
+
+// var a = prompt("Enter first number");
+// var b = prompt("Enter seconed number")
+
+// if(a>b){
+//     document.write(`${a} is larger number`)
+// }else if(a<b){
+//     document.write(`${b} is larger number`)
+// }else if(a==b){
+//     document.write(`both integers are equal`)
+// }
+
+                    // question 03
+//  Write a program that takes input a number from user & 
+// state whether the number is positive, negative or zero.
+
+// var c= prompt("Enter a number")
+// if(c<0){
+//     document.write(`${c} is negative`)
+// }else if(c>0){
+//     document.write(`${c} is positive`)
+
+// }else if(c==0){
+//     document.write(`${c} is zero`)
+// }
+
+
+                    // question 04
+// Write a program that takes a character (i.e. string of 
+// length 1) and returns true if it is a vowel, false otherwise
+
+// var d=prompt("Enter a alphabet")
+// if(d.length==1){
+//     if (d == 'a' || d == 'e' || d == 'i' || d == 'o' || d == 'u') {
+//         document.write(`${d} is a vowel`);
+//     } else {
+//         document.write(`${d} is not a vowel`);
+//     }
+// }else{
+//     document.write(`${d} is word not a alphabet`)
+// }
+
+
+
+                    //   question 05
+
+// // Step a: Store correct password in a JS variable.
+// var correctPassword = "zubairahmed123";
+
+// // Step b: Asks user to enter his/her password
+// var userPassword = prompt("Enter your password:");
+
+// // Step c: Validate the two passwords
+// if (userPassword === null) {
+//     // i. Check if user has entered password.
+//     alert("Please enter your password");
+// } else if (userPassword === correctPassword) {
+//     // ii. Check if both passwords are same.
+//     alert("Correct! The password you entered matches the original password.");
+// } else {
+//     alert("Incorrect password!");
+// }
+
+
+
+                    // question 06
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = ("Good day");
+// }
+// else{
+// greeting = ("Good evening");
+// }
+            
+
+                    // question 07
+
+// var time = prompt("Enter time in 24 houre clock format between 0000 and 2359");
+// if(time >= 0 && time <= 1200){
+//    alert("Good Morning")
+// }else if(time >= 1200 && time <=1700){
+//     alert("Good afternoon!")
+// }else if(time >= 1700 && time < 2100){
+//     alert("Good evening")
+// }else if(time >= 2100 && time <= 2359){
+//     alert("Good night!")
+// }
+
+                    
