3425e7ed6b1b6d12cfd7e632955c38430323cdcd
Syed Zubair hashmi <156568348+SyedZubairHashmi@users.noreply.github.com>
2024-03-20 10:05:17 +0500

    Add files via upload

---
+++ Assigment9_11/Assigment9_11.js
@@ -0,0 +1,183 @@
+                                                    // Question 01
+// var city = prompt("Enter your city name")
+// if(city == "karachi"){
+//     document.write(`City of light`)
+// }else if(city == "lahore"){
+//     document.write("city of colleges")
+// }else if(city == "peshawer"){
+//     document.write("city of flower")
+// }else if(city=="quetta"){
+//     document.write("Little paris")
+// }else{
+//     document.write("Sorry this city is not our list")
+// }
+      
+                                                    // Question 02
+
+// var gender = prompt("Enter yur Gender");
+// if(gender == "male"){
+//     document.write(`Good Morning Sir`)
+// }else if( gender == "female"){
+//     document.write(`Good Morning Ma'am`)
+// }                                    
+
+
+                                                    // Question 03
+
+// var trafficSignal = prompt("Enter signal color")   
+// if(trafficSignal == "red"){
+//     document.write(`Must Stop`)
+// }else if(trafficSignal == "yellow"){
+//     document.write(`Ready to move`)
+// }else if(trafficSignal == "green"){
+//     document.write(`Move now`)
+// }
+
+
+                                                    // Question 04
+// var remainingFuel = parseFloat(prompt("Enter the remaining fuel in your car (in litres):"));
+// if( remainingFuel < 0.25 ){
+//     alert(`Please refill the fuel in your car`)
+// }else{
+//     alert("fuel is enough in your car")
+// }
+
+
+                                                    // Question 05
+// var a = 4;
+// if(++a===5){
+//     alert("given condition for variable a is true")
+// }  
+
+// var b = 82;
+// if(b++ === 83){
+//     alert("given condition for variable b is true")
+// }else{
+//     alert("condition for b is false")
+// }
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+// if (true){
+//     alert("True");
+// }
+// if (false){
+//     alert("False");
+// }
+
+
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+// }
+                                                      //  Question 06
+// var math = parseInt(prompt("Enter your math marks"))
+// var english = parseInt(prompt("Enter your english marks"))
+// var physics = parseInt(prompt("Enter your physics marks"))
+// var total = 300;
+// var obtainedMarks = math + english + physics;
+// var percentage = (obtainedMarks/300)*100;
+// document.write(`Total marks : ${total}<br>`)
+// document.write(`Marks Obtained :  ${obtainedMarks}<br>`)
+// document.write(`Percentage : ${percentage}%<br>`)
+// if(percentage >= 80 && percentage <=100){
+//     document.write(`Grade : A-one <br>`)
+//     document.write(`Excellent`)
+// }else if(percentage >= 70 ){
+//     document.write(`Grade : A <br>`)
+//     document.write(`Good`)
+// }else if(percentage >= 60 ){
+//     document.write(`Grade : B <br>`)
+//     document.write(`You need to improve`)
+// }else if(percentage < 60){
+//     document.write(`Fail<br>`)
+//     document.write(`Sorry`)
+// }
+
+
+
+                                // Guess game
+                                                       //  Question 07
+// var secret = 6;
+// var guessNumber = prompt("guess a number")
+// if(guessNumber == secret){
+//     document.write("Bingo!Correct answer")
+// }else if(guessNumber == +secret  ){
+
+//      document.write(`Close enough to the correct answer`)     
+// }else{
+//     document.write("You can't guess  Sorry!")
+// }
+
+
+
+                                                        //  Question 08
+// var number = prompt("Enter a number")
+// var result = number%3;
+// if(result == 0){
+//     document.write(`The given number is divisible by 3`)
+// }else{
+//     document.write(`Sorry The given number is not divisible by 3`)
+// }
+
+                                                         //  Question 09
+// var givenNumber = prompt("Enter any number")
+// var result = givenNumber%2;
+// if(result==0 ){
+//     document.write(`${givenNumber} is Even number`)
+// }else{
+//     document.write(`${givenNumber} is odd number`)
+// }
+
+
+                                                         //  Question 10
+// var temperature = prompt("Enter your area temperature");
+// if(temperature > 40 ){
+//     document.write(`It is too hot outside`)
+// }else if(temperature > 30){
+//     document.write(`The Weather today is normal`)
+// }else if(temperature > 20 ){
+//     document.write("Today's Weather is cool")
+// }else if(temperature > 10){
+//     document.write("OMG! Today Weather is so Cool")
+// }
+
+
+                                                         //  Question 11
+// var firstNumber = parseInt(prompt("Enter first Number for Operation"))
+// var seconedNumber = parseInt(prompt("Enter seconed Number for Operation"))
+// var operation = prompt("Enter Operation  +  -  *  /  %  to performe")
+// if(operation == "+"){
+//     result = firstNumber + seconedNumber;
+//     document.write(`${firstNumber} + ${seconedNumber} = ${result} :`)
+// }else if(operation == "-"){
+//     result = firstNumber - seconedNumber;
+//     document.write(`${firstNumber} - ${seconedNumber} = ${result} :`)
+// }else if(operation == "/"){
+//     result = firstNumber/seconedNumber;
+//     document.write(`${firstNumber} / ${seconedNumber} = ${result} :`)
+// }else if(operation == "*"){
+//     result = firstNumber*seconedNumber;
+//     document.write(`${firstNumber} * ${seconedNumber} = ${result} :`)
+// }else if(operation == "%"){
+//     result = firstNumber % seconedNumber;
+//     document.write(`${firstNumber} % ${seconedNumber} = ${result} :`)
+// }
