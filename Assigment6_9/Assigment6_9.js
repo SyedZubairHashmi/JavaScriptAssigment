be9f1109a0ca1e170a36313c3827ebabd273bcf1
Syed Zubair hashmi <156568348+SyedZubairHashmi@users.noreply.github.com>
2024-03-20 06:11:48 +0500

    Add files via upload

---
+++ Assigment6_9/Assigment6_9.js
@@ -0,0 +1,93 @@
+                                        // Question 01
+// var a = 10;
+
+// document.write(`The value of a is : ${a}<br>`);
+// document.write(`===================<br>`);
+
+// ++a;
+// document.write(`The value of ++a is : ${a}<br>`);
+// document.write(`Now the value of a is : ${a}<br>`);
+
+// a++;
+// document.write(`The value of a++ is : ${a}<br>`);
+// document.write(`Now the value of a is : ${a}<br>`);
+
+// --a;
+// document.write(`The value of --a is : ${a}<br>`);
+// document.write(`Now the value of a is : ${a}<br>`);
+
+// a--;
+// document.write(`The value of a-- is : ${a}<br>`);
+// document.write(`Now the value of a is : ${a}<br>`);
+
+
+                                        // Question 02
+
+// var a = 2, b=1;
+
+// var result = --a - --b + ++b +  b--;
+
+
+// document.write(`a is ${a} <br> `);
+// document.write(`b is ${b} <br> `);
+// document.write(`result is ${result}<br>`)
+
+
+                        //  Question 03
+// var userName = prompt(`Enter your name`)
+// document.write(`Happy to meet you ${userName}`);
+
+
+                        //  Question 05
+// var table = prompt("Enter a number to print  a table");
+// if(table === undefined){
+//     table = 5;
+//     document.write(`${table}x1=${table*1}<br>`)
+//     document.write(`${table}x1=${table*1}<br>`)
+//     document.write(`${table}x1=${table*1}<br>`)
+//     document.write(`${table}x2=${table*2}<br>`)
+//     document.write(`${table}x2=${table*2}<br>`)
+//     document.write(`${table}x2=${table*2}<br>`)
+//     document.write(`${table}x3=${table*3}<br>`)
+//     document.write(`${table}x4=${table*4}<br>`)
+//     document.write(`${table}x5=${table*5}<br>`)
+//     document.write(`${table}x6=${table*6}<br>`)
+//     document.write(`${table}x7=${table*7}<br>`)
+//     document.write(`${table}x8=${table*8}<br>`)
+//     document.write(`${table}x9=${table*9}<br>`)
+//     document.write(`${table}x10=${table*10}<br>`)
+// }else{
+
+//     document.write(`${table}x1=${table*1}<br>`)
+//     document.write(`${table}x1=${table*1}<br>`)
+//     document.write(`${table}x1=${table*1}<br>`)
+//     document.write(`${table}x2=${table*2}<br>`)
+//     document.write(`${table}x2=${table*2}<br>`)
+//     document.write(`${table}x2=${table*2}<br>`)
+//     document.write(`${table}x3=${table*3}<br>`)
+//     document.write(`${table}x4=${table*4}<br>`)
+//     document.write(`${table}x5=${table*5}<br>`)
+//     document.write(`${table}x6=${table*6}<br>`)
+//     document.write(`${table}x7=${table*7}<br>`)
+//     document.write(`${table}x8=${table*8}<br>`)
+//     document.write(`${table}x9=${table*9}<br>`)
+//     document.write(`${table}x10=${table*10}<br>`)
+// }
+
+                        //  Question 06
+
+// var english = parseInt(prompt("Enter English marks obtained"));
+// var math = parseInt(prompt("Enter Math marks obtained"));
+// var urdu = parseInt(prompt("Enter Urdu marks obtained"));
+// var total = 100;
+// document.write("Subject Total Marks Obtained Marks Percentage <br>")
+// var englishp = english/total*100;
+// var mathp = math/total*100;
+// var urdup =urdu/total*100;
+// var totalMarks = english + math + urdu;
+// document.write(`English  ${total}   ${english}   ${englishp}%<br> `)
+// document.write(`Math  ${total}   ${math}   ${mathp}% <br>`)
+// document.write(`Urdu  ${total}   ${urdu}   ${urdup}% <br>`)
+// document.write(`          ${total+total+total}     ${totalMarks}    ${(urdup+englishp+mathp)/3}%  `)
+
+
