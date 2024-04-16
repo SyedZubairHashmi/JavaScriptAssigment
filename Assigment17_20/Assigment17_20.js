2c0b2074f8263a0efbd8a1ce2f74ad69e7daac91
Syed Zubair hashmi <156568348+SyedZubairHashmi@users.noreply.github.com>
2024-04-16 15:34:44 -0700

    Add files via upload

---
+++ Assigment17_20/Assigment17_20.js
@@ -0,0 +1,140 @@
+                                //  Qustion 01
+// var multyArray  = [[], [] ]
+
+                                //  Qustion 02
+// var matrix = [[ 0, 1,2,3] ,[1,0,1,2], [2,3,0,1] ]  
+// document.write(`${matrix[0] }<br>`)     
+// document.write(`${matrix[1] }<br>`)  
+// document.write(`${matrix[2] }<br>`)                   
+
+
+                                //  Qustion 03
+
+// for(var i=1; i<=10;i++){
+//     document.write(`${i}<br>`)
+// }
+
+                                //  Qustion 04
+// var table = prompt("Enter a number to show its multiplication table")
+// var lengthTable = prompt("Enter length multiplication table  ")
+// document.write(`Multiplication table of ${table}<br>`)
+// document.write(`Lenght ${lengthTable}<br>`)
+// for(var i=1; i<=lengthTable; i++){
+       
+//     document.write(`${table}x${i}=${table*i}<br>`)
+
+// }
+
+
+// var fruits = ["apple","banana","mango","orange","strawberry"]
+//  for(var i=0; i<fruits.length; i++){
+//     document.write(`${fruits[i]}<br> `)
+//  }
+
+//   document.write(`<br>`)
+//  for (var i=0; i<fruits.length; i++){
+//     document.write(`Element at index ${i} is ${fruits[i]}<br>`)
+//  }
+
+                                //  Qustion 06
+//     document.write(`Counting:`)
+// {
+// for(var i=0; i< 16; i++){
+//     document.write(`${i},`)
+// }
+// }
+//     document.write(`Reverse Counting: `)
+// {
+// for(var i=10; i>0; i--){
+//     document.write(`${i},`)
+// }
+// }
+//     document.write(` Even: `)
+// {
+//     document.write(` Even: `)
+//  for(var i=0; i<=20; i++){
+
+//     var even = i%2;
+//     if(even === 0){
+//         document.write(`${i},`)
+//     }
+
+//   }
+// }
+//     document.write(`<br>Odd: `)
+// {
+//  for(var i=0; i<=20; i++){
+
+//     var even = i%2;
+//     if(even != 0){
+//         document.write(`${i},`)
+//     }
+
+//   }
+// }
+// document.write(`Series: `)
+// {
+//  for(var i=0; i<=20; i++){
+//     var even = i%2;
+//     if(even === 0){
+//         document.write(`${i}k,`)
+//     }
+//   }
+// }
+
+                                //  Qustion 07
+                              
+// var resturent =  ["cake", "apple pie", "cookie", "chips", "patties"];
+
+// var userInput = prompt("Welcome to ABC Bakery. What do you want to order sir/ma'am")
+// if(userInput == "cake"){
+//   document.write(`${resturent[0] } is availaible at index ${resturent.indexOf('cake')}  in our bakery`)
+// }else if(userInput == "apple pie"){
+//   document.write(`${resturent[1]} is availaible at index ${resturent.indexOf('apple pie')} in our bakery  `)
+// }else if(userInput == "cookie"){
+//   document.write(`${resturent[2]} is availaible at index ${resturent.indexOf('cookie')} in our bakery  `)
+// }else if(userInput == "chips"){
+//   document.write(`${resturent[3]} is availaible at index ${resturent.indexOf('chips')}  in our bakery `)
+// }else if(userInput == "patties"){
+//   document.write(`${resturent[4]} is availaible at index ${resturent.indexOf('patties')}  in our bakery `)
+// }
+
+// else{
+//   document.write(`We are sorry. ${userInput} is not available in our bakery`)
+// }
+       
+
+
+                                //  Qustion 08
+// var numbers = [24, 53, 78, 91, 12]
+// var maxNum = numbers[0];
+// for(var i=1; i<numbers.length; i++){
+  
+//    if(numbers[i]>maxNum){
+//     maxNum = numbers[i];
+//    }
+// }
+// document.write(`The largest number in the array is ${maxNum}.`)
+
+
+
+
+                                //  Qustion 09
+// var numbers2 = [24, 53, 78, 91, 12]
+// var minNum = numbers2[0];
+// for(var i=1; i<numbers2.length;i++){
+//   if(minNum>numbers2[i]){
+//    minNum = numbers2[i];
+//   }
+// }
+
+// document.write(`The smallest number in the array is ${minNum}.`)
+
+
+
+                                //  Qustion 10
+// for(var i=1; i<=100; i++){
+//    if(i % 5 === 0){
+//     document.write(`${i},`)
+//    }
+// }
